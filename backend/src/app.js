e7ae4ed177089a2d78ccf3fcf60d8b18413f2052
nk112233 <nikunjkadu19@gmail.com>
2024-04-09 13:12:53 +0530

    Added Deployment Changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import path from 'path';
 
+
+
+const __dirname = path.resolve();
 const app = express();
 
 app.use(cors({
@@ -13,7 +17,11 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 //routes import
 import userRouter from './routes/user.routes.js'
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,6 @@
 import connectDB from "./db/index.js";
 import {app} from './app.js'
 
-
-
-
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT||8000, ()=>{
