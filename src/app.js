9cffb5f0662a0d0d78308dcd9b7fcdbc418950eb
nk112233 <nikunjkadu19@gmail.com>
2024-04-11 18:32:53 +0530

    Minor change

--- src/app.js
+++ src/app.js
@@ -38,9 +38,9 @@ app.use("/api",likesRouter)
 app.use("/api",eventRouter)
 app.use("/api",answerRouter)
 
-app.use(express.static(path.join(__dirname , '/client/dist')))
+app.use(express.static(path.join(__dirname , '../client/dist')))
 
-app.get('*' , (req , res) => res.sendFile(path.join(__dirname , '/client/dist/index.html')))
+app.get('*' , (req , res) => res.sendFile(path.join(__dirname , '../client/dist/index.html')))
 
 
 app.listen(3001 , () => {
